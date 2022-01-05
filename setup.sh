654cf853109d3d22deb386af077ad5821684ea1c
Katayama Yutaro <taro710kata@gmail.com>
2022-01-05 15:15:59 +0900

    add: setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+####################
+# homebrewのセットアップ
+####################
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+
+####################
+# vimのセットアップ
+####################
+
 mkdir -p $HOME/.vim/pack/myplugins/start
 mkdir -p $HOME/.vim/pack/myplugins/opt
 
@@ -19,7 +30,23 @@ git clone https://github.com/sheerun/vim-polyglot.git
 echo '=======lightline.vim.gitインストール開始======='
 git clone https://github.com/itchyny/lightline.vim.git
 
-
-
 # -------------- 任意タイミング（:packadd myplugin）でロードされるプラグイン
 cd $HOME/.vim/pack/myplugins/opt
+
+
+####################
+# preztoのセットアップ
+####################
+
+echo 'preztoインストール開始'
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+
+####################
+# アプリのセットアップ
+####################
+
+echo 'アプリインストール開始'
+
+brew bundle install
